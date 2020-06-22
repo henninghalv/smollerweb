e77833afbd2bc496071c4f1b943655ebc9d833d7
Henning Bang Halvorsen <henning.b.halvorsen@gmail.com>
2020-06-22 10:12:39 +0200

    Move animation js to js file, change favicon

---
+++ frontend/js/main.js
@@ -0,0 +1,8 @@
+// Animation
+let animation = bodymovin.loadAnimation({
+  container: document.getElementById('animation'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/smollerAnimation.json',
+});
