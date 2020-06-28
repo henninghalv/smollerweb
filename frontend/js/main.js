cbae6fe728abaf29ad4676d5f89300811657b2f8
Henning Bang Halvorsen <henning.b.halvorsen@gmail.com>
2020-06-28 14:57:49 +0200

    Create mobile navigation menu, tidy up assets folder

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -7,4 +7,18 @@ let animation = bodymovin.loadAnimation({
   path: 'js/smollerAnimation.json',
 });
 
+// If JS is enabled, these become visible
 document.getElementById('animation').style.display = 'flex';
+document.getElementById('openMobile').style.display = 'block';
+
+function openMobileNav() {
+  document.getElementById('openMobile').style.display = 'none';
+  document.getElementById('closeMobile').style.display = 'block';
+  document.getElementById('mobileNavBar').style.display = 'flex';
+}
+
+function closeMobileNav() {
+  document.getElementById('openMobile').style.display = 'block';
+  document.getElementById('closeMobile').style.display = 'none';
+  document.getElementById('mobileNavBar').style.display = 'none';
+}
